d5ee1c0e2c5a6411aa25135907d3e27193813b47
Archipp <armunda2020@gmail.com>
2023-12-26 15:54:07 +0100

    first commit

---
+++ app/(modals)/booking.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const Page = () => {
+  return (
+    <View>
+      <Text>BOOK</Text>
+    </View>
+  )
+}
+
+export default Page
